f32747e8a95ef4d40fb9879ed9b8c7de23f983e7
jbaraiya <jbaraiya254@rku.ac.in>
2020-07-31 00:17:47 +0530

    complete splesh screen

--- app/src/main/java/com/rku/groupproject5ce_4/SpleshScreen.java
+++ app/src/main/java/com/rku/groupproject5ce_4/SpleshScreen.java
@@ -1,23 +1,22 @@
 package com.rku.groupproject5ce_4;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SpleshScreen extends AppCompatActivity {
 
 
 
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 
 
 
@@ -26,7 +25,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
         getSupportActionBar().hide();
 
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+
+
+
+
+                startActivity(new Intent(SpleshScreen.this,MainActivity.class));
+                finish();
+
+            }
+
+        },3000);
+
+
     }
+
 }
