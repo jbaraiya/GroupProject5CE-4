dbeb855a7803858d360c4931c8a8b5b61e30c042
jbaraiya <jbaraiya254@rku.ac.in>
2020-07-31 20:27:35 +0530

    xml files added

--- app/src/main/java/com/rku/groupproject5ce_4/MainActivity.java
+++ app/src/main/java/com/rku/groupproject5ce_4/MainActivity.java
@@ -2,13 +2,41 @@ package com.rku.groupproject5ce_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        getSupportActionBar().hide();
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+
+
+
+
+                startActivity(new Intent(MainActivity.this,MainActivity.class));
+                finish();
+
+            }
+
+        },3000);
         setContentView(R.layout.activity_main);
     }
 }
