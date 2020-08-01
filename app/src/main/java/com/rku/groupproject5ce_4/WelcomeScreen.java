659639e0193cb76cabb7d5151edb927363bf923c
jbaraiya <jbaraiya254@rku.ac.in>
2020-08-01 19:19:29 +0530

    ActivityMain.java java code added

---
+++ app/src/main/java/com/rku/groupproject5ce_4/WelcomeScreen.java
@@ -0,0 +1,35 @@
+package com.rku.groupproject5ce_4;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
+}
